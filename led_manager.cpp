7ad3b1972202dd9b0d6406b3c6e29aab3c321490
Tsushima <b1019217@fun.ac.jp>
2024-05-19 20:16:34 +0900

    クラス統合

--- led_manager.cpp
+++
@@ -1,8 +0,0 @@
-#include "led_manager.h"
-led_manager::led_manager() {
-  M5.begin(true, false, true);
-}
-
-void led_manager::set_color(CRGB color) {
-  M5.dis.drawpix(0, (color.g << 16) | (color.r << 8) | color.b);
-}
--- m5_motor_controller.ino
+++ m5_motor_controller.ino
@@ -1,19 +1,40 @@
-#include "led_manager.h"
+#include <Ps3Controller.h>
+#include <M5Atom.h>
 #include "mortor_driver.h"
 
-led_manager l;
 mortor_driver m;
 
 void setup() {
-  m.write_vset(DRV2_ADR, MIN_VSET, M_STANBY); 
+  Serial.begin(115200);
+
+  //mortor driver init
+  m.write_vset(DRV2_ADR, MIN_VSET, M_STANBY);
+
+  //led init
+  M5.begin(true, false, true);
+
+  //dualshock3 init
+  Ps3.begin("4C:75:25:CE:39:A6");
+  Ps3.setPlayer(0);
+
+  Serial.println("Ready.");
 }
 void loop() {
-  l.set_color(CRGB::Red);
+  if (!Ps3.isConnected()) {
+    M5.dis.drawpix(0, 0, 0);
+    delay(100);
+    M5.dis.drawpix(0, 0, 225);
+    delay(100);
+    
+    return;
+  }
+
+  //l.set_color(CRGB::Red);
   m.write_vset(DRV2_ADR, MAX_VSET, M_NORMAL);
   m.write_vset(DRV1_ADR, MAX_VSET, M_NORMAL);
   delay(3000);
-  l.set_color(CRGB::Blue);
+  //l.set_color(CRGB::Blue);
   m.write_vset(DRV2_ADR, MAX_VSET, M_REVERSE);
-  m.write_vset(DRV1_ADR, MAX_VSET, M_NORMAL);
+  m.write_vset(DRV1_ADR, MAX_VSET, M_REVERSE);
   delay(3000);
 }
